a3d9941100dd69a919c5d9ea35a6a5fc10c67270
jaid <mjzaid921.pccoer@gmail.com>
2020-07-08 08:37:33 +0530

    v1.0

---
+++ public/frontend.js
@@ -0,0 +1,42 @@
+// Make connection 1.6
+var socket = io.connect('http://localhost:4000');
+
+// Query DOM 1.7
+var message = document.getElementById('message'),
+username = document.getElementById('username'),
+      send = document.getElementById('send'),
+      conversation = document.getElementById('conversation'),
+      feedback = document.getElementById('feedback');
+var ele=document.getElementById("new");
+
+//1.8.0
+message.addEventListener('keypress', function(){
+    socket.emit('typing', username.value);
+});
+//1.8.3
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' typing a message...</em></p>';
+});
+
+// Emit events 1.8.4
+send.addEventListener('click', function(){
+    //1.8.5
+    socket.emit('chat', {
+        message: message.value,
+        username: username.value
+    });
+    message.value = "";
+});
+
+// Listen for events 1.8.8
+socket.on('chat', function(data){
+    feedback.innerHTML = '';
+    if(data.username==''){
+        data.username='You';
+    }
+    conversation.innerHTML += '<p><strong>' + data.username + ': </strong>' + data.message + '</p>';
+    ele.scrollIntoView();
+});
+
+
+
---
+++ server.js
@@ -0,0 +1,80 @@
+//packages used 1.1
+const path=require('path');
+const express = require('express');
+const socket = require('socket.io');
+
+const publicPath=path.join(__dirname,"/public");
+console.log(publicPath);
+// App setup 1.2
+var app = express();
+const PORT=process.env.PORT || 4000;
+var server = app.listen(PORT, function(){
+    console.log('server setup done PORT:',PORT);
+});
+
+// Static files 1.3
+app.use(express.static(publicPath));
+
+// Socket setup & pass server 1.4
+var io = socket(server);
+io.on('connection', (socket) => {
+    //1.5
+    console.log('Connected Client : ', socket.id);
+    botreply.message=menu;
+    socket.emit('chat',botreply);
+
+    // Handle typing event 1.8.1
+    socket.on('typing', function(data){
+        //1.8.2
+        //socket.broadcast.emit('typing', data);
+        socket.emit('typing', data);
+    });
+
+    // Handle chat event 1.8.6
+    socket.on('chat', function(data){
+        // console.log(data); 1.8.7
+        //io.sockets.emit('chat', data);
+        socket.emit('chat',data);
+        chatbotReply(data);
+        socket.emit('chat',botreply);
+    });
+
+});
+
+//CHAT BOT
+var menu=["Hi, how may I help you?<br/>","Please reply with the numbers to the corresponding questions.<br/> 1.what is IP? <br/>2. How is IP? <br/>3.Bitrix24 Issues <br/>4.IP training issues <br/> 5.Jobs available"];
+var rep1="Watch the below video <br/> URL: <iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/OnKfrQrEOrk\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe> <br/> Need any more assistance reply with *";
+var rep2="Watch the below video <br/> URL: <iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Hs9npUUIg4I\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe> <br/> Need any more assistance reply with * ";
+var rep3="Please reply with the corresponding <br/> Question number to get your queries Cleared.<br/> 1.question1 <br/> 2.Question2 <br/> 3.Question3 <br/> 4.Question4 <br/> 5.Question5 <br/> Reply with * to go back to the previous menu";
+var rep3_1="Answer to question 1: <br/> Reply with * to go back to the previous menu.";
+var rep3_2="Answer to question 2: <br/> Reply with * to go back to the previous menu.";
+var rep3_3="Answer to question 3: <br/> Reply with * to go back to the previous menu.";
+var rep3_4="Answer to question 4: <br/> Reply with * to go back to the previous menu.";
+var rep3_5="Answer to question 5: <br/> Reply with * to go back to the previous menu.";
+var rep4_link="https://docs.google.com/document/d/1fCdmvD2oaQ-9gxW8LN4SYazK_rfx2ylX_g3-4V7ujtc/edit";
+var rep5_link="<a href=\"https://docs.google.com/document/d/1hvUpHJH5L8UTyets-gw8XYZ-gIv2jzofM3I6Uy7HuO8/edit\" target=\"_blank\">click here</a>";
+var wrongChoice="";
+var chatbot_reply=[menu,]
+var botreply={
+    username:'BOT',
+    message:''
+}
+function chatbotReply(data){
+    console.log(data);
+    if(data.message=='1'){
+        botreply.message=rep1;
+    }else if(data.message=='2'){
+        botreply.message=rep2;
+    }else if(data.message=='3'){
+        botreply.message=rep3;
+        //chatbotSubReply(data);
+    }else if(data.message=='4'){
+        botreply.message=rep4_link;
+    }else if(data.message=='5'){
+        botreply.message=rep5_link;
+    }else if(data.message=='*'){
+        botreply.message=menu[1];
+    }else{
+        botreply.message=menu[1];
+    }
+}
